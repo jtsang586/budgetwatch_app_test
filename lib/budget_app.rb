5fb6a1b39670dfde63e54e6b4415674d99c75c7e
Melinda Fernandes <melindafernandes1993@gmail.com>
2017-10-04 10:54:43 +0100

    budget page object model

--- lib/budget_app.rb
+++ lib/budget_app.rb
@@ -1,9 +1,14 @@
 require_relative 'pages/main_page'
+require_relative 'pages/budget_page'
 
 module BudgetApp
 
   def mainpage
     MainPage.new($driver)
+  end
+
+  def budgetpage
+    BudgetPage.new($driver)
   end
 
 end
